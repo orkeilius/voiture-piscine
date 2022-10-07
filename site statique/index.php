9652fef8f524e221ec639a483cccf13a727039e0
orkeilius <orkeilius@users.noreply.github.com>
2022-10-07 15:54:35 +0200

    add header and footer

---
+++ site statique/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <header>
+
+    </header>
+    <body>
+        <?php include("module/header.php") ?>
+
+
+        <?php include("module/footer.php") ?>
+    </body>
+</html>
