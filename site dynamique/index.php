e8009a1f5fbfc41b4d5c2cbd209c17e95d452339
orkeilius <orkeilius@users.noreply.github.com>
2022-10-08 17:32:16 +0200

    copy basic file to "site dynamique"

---
+++ site dynamique/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include("module/head.php") ?>
+    </head>
+    <body>
+        <?php include("module/header.php") ?>
+
+
+        <?php include("module/footer.php") ?>
+    </body>
+</html>
---
+++ site dynamique/module/header.php
@@ -0,0 +1,7 @@
+<header>
+    <h1>Voitures et Piscines</h1>
+    <nav>
+        <a href="accueil">Accueil</a>
+        <a href="profile">Profile</a>
+    </nav>
+</header>
