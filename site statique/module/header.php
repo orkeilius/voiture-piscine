5cccb5d5bfa5a1016e01c5017a15d126d432a264
DRNBSC <115243935+DRNBSC@users.noreply.github.com>
2022-10-08 16:43:00 +0100

    liens et titre

---
+++ site statique/module/header.php
@@ -0,0 +1,8 @@
+<header>
+    <h1>Voitures et Piscines</h1>
+    <nav>
+        <a href="http://voiture-piscine.com/accueil/">Accueil</a>
+        <a href="http://voiture-piscine.com/produits/">Nos produits</a>
+        <a href="http://voiture-piscine.com/CONTACT/">Contactez-nous</a>
+    </nav>
+</header>
