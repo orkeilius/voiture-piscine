2e0e35608b497412bca7e1e893f0b56acbd138c4
orkeilius <orkeilius@users.noreply.github.com>
2022-10-08 19:02:28 +0200

    add beta login

--- site dynamique/index.php
+++ site dynamique/index.php
@@ -1,12 +1,27 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <?php include("module/head.php") ?>
-    </head>
-    <body>
-        <?php include("module/header.php") ?>
 
+<head>
+    <?php include("module/head.php") ?>
+</head>
 
-        <?php include("module/footer.php") ?>
-    </body>
+<body>
+    <?php include("module/header.php"); ?>
+    <section>
+        <?php # placeholder for login
+        if (False) {  ?>
+        <h1>dernier message</h1>
+        <?php } 
+        else{?>
+            <h2>log in</h2>
+            <form>
+                <input type="text" class="entry" id="user" name="user" placeholder="username" required><br>
+                <input type="password" class="entry" id="password" name="password" placeholder="password" required><br>
+                <input type="submit" value="se connecter">
+            </form>
+        <?php }; ?>
+    </section>
+    <?php include("module/footer.php") ?>
+</body>
+
 </html>
