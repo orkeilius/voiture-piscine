e8ee32cd990ef6a23e2babbf9fe7281f36e19fba
orkeilius <orkeilius@users.noreply.github.com>
2022-10-14 13:49:57 +0200

    add favicon on both site

--- site dynamique/module/head.php
+++ site dynamique/module/head.php
@@ -1,2 +1,3 @@
 <link rel="stylesheet" href="main.css">
 <meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="icon" type="image/ico" href="favicon.png">
--- site statique/module/head.php
+++ site statique/module/head.php
@@ -1,1 +1,2 @@
 <link rel="stylesheet" href="main.css">
+<link rel="icon" type="image/ico" href="favicon.png">
