c4ea48a00b99695a33e7adb67ddb47c4ef95010d
orkeilius <orkeilius@users.noreply.github.com>
2022-10-14 16:20:02 +0200

    add "add article button"

--- site dynamique/module/header.php
+++ site dynamique/module/header.php
@@ -1,14 +1,19 @@
+<?php include 'dbTools.php'; ?>
 <header>
     <h1>Vp enterprise</h1>
     <nav>
         <a class="navItem" href="/">Accueil</a>
-        <?php if(isset($_SESSION["user"])){ ?>
+        <?php if (isset($_SESSION["user"])) {
+            if (getUserAccess() <= 2) { ?>
+                <a class="navItem" href="addArticle"> + ajouter un article</a>
+            <?php } ?>
+            <div style="flex-basis:1;flex-grow:100"></div>
             <div class="dropdown">
                 <button class="navItem" href="profile">Profil</button>
-            <div>
-                <a class="dropdownItem" href="option.php">Option</a>
-                <a class="dropdownItem" href="logout.php">Logout</a>
-            </div>
+                <div>
+                    <a class="dropdownItem" href="option.php">Option</a>
+                    <a class="dropdownItem" href="logout.php">Logout</a>
+                </div>
 
             </div>
         <?php } ?>
