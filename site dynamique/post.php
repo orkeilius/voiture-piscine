09d95681062c5c81daee5ea8e6cd7bb227ac0f21
orkeilius <orkeilius@users.noreply.github.com>
2022-11-08 16:35:48 +0100

    add post query

--- site dynamique/post.php
+++ site dynamique/post.php
@@ -12,9 +12,24 @@
     if (getUserAccess() > 3 || $id == false ) {
         header("Location: /");
     } else {
+        $query = $db->prepare("SELECT * FROM `post` WHERE `postId`=?");
+        $query->bindParam(1,$id);
+        $query->execute();
+        $result = $query->fetch();
+
+
+
         include("module/header.php"); ?>
-
-
+        <section>
+            <article class="itemWarper">
+                <h2><?php echo $result["postTitle"] ?></h2>
+                <h4><?php echo $result["userName"] ?></h4>
+                <p>
+                    <?php echo $result["postContent"] ?>
+                </p>
+            </article>
+        </section>
+        
     <?php include("module/footer.php");
     }
     ?>
