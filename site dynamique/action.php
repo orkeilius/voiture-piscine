667ff4e39f04e99b6ac4b9b33050a4a804f127eb
orkeilius <orkeilius@users.noreply.github.com>
2022-11-09 10:37:51 +0100

    add timestamp

--- site dynamique/action.php
+++ site dynamique/action.php
@@ -15,10 +15,11 @@ elseif ($_POST["action"] == "addArticle"){
         header("Location: addArticle.php?error=option");
     }
     else{
-        $query = $db -> prepare("INSERT INTO `post`(`postId`, `postTitle`, `postContent`, `postTime`, `postPinned`, `userName`) VALUES (NULL,?,?,NULL,0, ? );");
+        $query = $db -> prepare("INSERT INTO `post`(`postId`, `postTitle`, `postContent`, `postTime`, `postPinned`, `userName`) VALUES (NULL,?,?,?,0, ? );");
         $query->bindParam(1,$result["title"]);
         $query->bindParam(2,$result["content"]); 
-        $query->bindParam(3,$_SESSION["user"]);
+        $query->bindParam(3,time()); 
+        $query->bindParam(4,$_SESSION["user"]);
         var_dump($query);
         $query->execute();
         $result =  $query->fetch();
