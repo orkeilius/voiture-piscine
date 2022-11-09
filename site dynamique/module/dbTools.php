ed4ae8a2c5051330fe9e1b0f7d4555809bfbb56b
orkeilius <orkeilius@users.noreply.github.com>
2022-11-09 13:03:00 +0100

    move checkPassword to dbTools

--- site dynamique/module/dbTools.php
+++ site dynamique/module/dbTools.php
@@ -1,8 +1,24 @@
 <?php
+if(! isset($_GET["error"])){
+    $_GET["error"] = NULL;
+}
 try{
     $db = new PDO('mysql:dbname=vp-enterprise;host=127.0.0.1' ,"root" , "");
 } catch(PDOException $e){
     die ("DB Error".$e);
+}
+function checkPassword($user, $password){
+    global $db;
+    $query = $db->prepare("SELECT * FROM `user` WHERE `userName` = ?;");
+    $query->bindParam(1,htmlspecialchars($user));
+    $query->execute();
+    $result = $query->fetch();
+
+    if (password_verify(htmlspecialchars($password), $result["userPassword"])){
+        $_SESSION["user"] = $result["userName"];
+        return true;
+    };
+    return false;
 }
 function getUserAccess(){
     if (! isset($_SESSION["user"])){
@@ -15,4 +31,3 @@ function getUserAccess(){
     $result =  $query->fetch();
     return $result["userAccess"];
 }
-
