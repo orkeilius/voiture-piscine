46fb40c735ca2e7ba5b9ce4da273f26ab51eafca
orkeilius <orkeilius@users.noreply.github.com>
2022-11-09 10:55:49 +0100

    fix action's name

--- site dynamique/option.php
+++ site dynamique/option.php
@@ -16,7 +16,7 @@
             <div class="itemWarper">
             <h2>changer de mot de passe</h2>
                 <form action="action.php" method="post">
-                    <input type='hidden' name='action' value='change password' />
+                    <input type='hidden' name='action' value='changePassword' />
                     <label for="old" >mot de passe actuel</label><br/>
                     <input type="password" name="old" pattern="^.{0,255}" required><br/>
                     <label for="new" >nouveau mot de passe</label><br/>
