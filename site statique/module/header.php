fe94cc6c7976c06e1db7620f71447ea4e3d16744
orkeilius <orkeilius@users.noreply.github.com>
2022-11-12 11:17:09 +0100

    fix "not working link on svg"

--- site statique/module/header.php
+++ site statique/module/header.php
@@ -1,7 +1,7 @@
 <header>
     <h1>Voitures-Piscines</h1>
     <nav>
-        <a href="index.php"><object data="img/home.svg" class="icon" type="image/svg+xml"></object></a>
+        <a href="index.php"><img src="img/home.svg" class="icon" alt="accueil" /></a>
         <a href="produits.php">Nos produits</a>
         <a href="presentation.php">Qui-sommes-nous ?</a>
     </nav>
