f02e920717e79780edeae99ee30dbcce5d83038b
orkeilius <orkeilius@users.noreply.github.com>
2022-11-12 11:01:22 +0100

    fix title

--- site statique/module/header.php
+++ site statique/module/header.php
@@ -1,5 +1,5 @@
 <header>
-    <h1>Voitures et Piscines</h1>
+    <h1>Voitures-Piscines</h1>
     <nav>
         <a href="index.php"><object data="img/home.svg" class="icon" type="image/svg+xml"></object></a>
         <a href="produits.php">Nos produits</a>
