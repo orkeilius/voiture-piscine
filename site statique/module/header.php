4b9410b1d67a4cca22f9247ddea7eadb690a21fd
orkeilius <orkeilius@users.noreply.github.com>
2022-11-12 10:51:09 +0100

    replace the logo in the nav to a svg

--- site statique/module/header.php
+++ site statique/module/header.php
@@ -1,7 +1,7 @@
 <header>
     <h1>Voitures et Piscines</h1>
     <nav>
-        <a href="index.php"><img src="img/logo.jpg" style="width:35px; height:35px" class="rond"></a>
+        <a href="index.php"><object data="img/home.svg" class="icon" type="image/svg+xml"></object></a>
         <a href="produits.php">Nos produits</a>
         <a href="presentation.php">Qui-sommes-nous ?</a>
     </nav>
