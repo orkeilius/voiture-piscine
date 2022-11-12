d094a3911b3be40da1c5c2cda56939965c8a3af5
orkeilius <orkeilius@users.noreply.github.com>
2022-11-12 11:46:57 +0100

    fix footer and add website plan

--- site statique/module/footer.php
+++ site statique/module/footer.php
@@ -1,10 +1,24 @@
 <footer>
-    Tous droits réservés © voiture-piscine .corp 2022<br>
-    Pour nous contacter, cliquez sur ces icones ci-dessous !<br>
-    <br>    
-    <nav id="pied">
-        <a href="https://twitter.com/?lang=fr"><img src="img/twt.png" style="width:35px; height:35px" class="rond"></a>
-        <a href="https://www.instagram.com/?hl=fr"><img src="img/insta.png" style="width:35px; height:35px" class="rond"></a>
-        <a href="https://www.facebook.com/"><img src="img/fb.png" style="width:35px; height:35px" class="rond"></a>
-    </nav>
+    <div class="footerSection">
+    <div class="footerArticle">
+            <h3>Voiture-Piscine.com</h3>
+            <ul>
+                <li><a href="/index.php">Accueil</a></li>
+                <li><a href="/produits.php">Nos Produit</a></li>
+                <li><a href="/presentation.php">Qui somme nous ?</a></li>
+            </ul>
+        </div>
+        <div class="footerArticle">
+            <h3>Nous contacter</h3>
+            <ul>
+                <li><a href="https://twitter.com/?lang=fr">Twitter</a></li>
+                <li><a href="https://www.instagram.com/?hl=fr">Instagram</a></li>
+                <li><a href="https://www.facebook.com/">Facebook</a></li>
+            </ul>
+        </div>
+
+    </div>
+    <p>
+        Tous droits réservés © voiture-piscine .corp 2022
+    </p>
 </footer>
--- site statique/module/head.php
+++ site statique/module/head.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="style/main.css">
 <link rel="stylesheet" href="style/header.css">
+<link rel="stylesheet" href="style/footer.css">
 <link rel="icon" type="image/ico" href="favicon.png">
 <meta name="viewport" content="width=device-width, initial-scale=1" />
