6217d9d3eafb0bea7522258c78edb5fadfb5aee9
orkeilius <orkeilius@users.noreply.github.com>
2022-11-12 11:14:51 +0100

    split css in multiple file

--- site statique/module/head.php
+++ site statique/module/head.php
@@ -1,3 +1,4 @@
-<link rel="stylesheet" href="main.css">
+<link rel="stylesheet" href="style/main.css">
+<link rel="stylesheet" href="style/header.css">
 <link rel="icon" type="image/ico" href="favicon.png">
 <meta name="viewport" content="width=device-width, initial-scale=1" />
