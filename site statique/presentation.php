cba0894eb7bc463e6794347bb25ce932cf0c76fc
orkeilius <orkeilius@users.noreply.github.com>
2022-11-15 11:16:01 +0100

    fix presentation

--- site statique/presentation.php
+++ site statique/presentation.php
@@ -1,19 +1,29 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <?php include("module/head.php") ?>
-    </head>
-    <body>
-        <?php include("module/header.php") ?>
-        <section>
-            <h2>Présentation</h2>
-            <p>
-                Nous sommes une entreprise établie en France dont le but est de produire et vendre des voitures piscines <br>
-                Nous effectuons aussi de la location de camion piscine et vendons également des kits de transformation pour les pickups <br>
-                Si vous avez plus de question, n'hésitez pas ! </br>
-                <img src="img/modif.jpg" style="width:250px; height:200px">
-            </p>
-        </section>
-        <?php include("module/footer.php") ?>
-    </body>
+
+<head>
+    <?php include("module/head.php") ?>
+    <link rel="stylesheet" href="style/presentation.css">
+</head>
+
+<body>
+    <?php include("module/header.php") ?>
+    <section>
+        <article>
+            <div>
+                <h2>qui somme nous ?</h2>
+                <p>
+                    Nous sommes une entreprise établie en France dont le but est de produire et vendre des voitures piscines <br>
+                    Nous effectuons aussi de la location de camion piscine et vendons également des kits de transformation pour les pickups <br>
+                    Si vous avez plus de question, n'hésitez pas ! </br>
+                </p>
+            </div>
+            <div style="mask-image: url(img/mask/blob4.svg);" class="mask">
+                <img src="img/modif.jpg">
+            </div>
+        </article>
+    </section>
+    <?php include("module/footer.php") ?>
+</body>
+
 </html>
