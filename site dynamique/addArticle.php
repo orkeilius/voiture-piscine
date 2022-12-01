1d5fb22f0f9524c2f965b53a5bb6bd6b1cb3534b
orkeilius <orkeilius@users.noreply.github.com>
2022-12-01 08:46:02 +0100

    add file-upload front-end

--- site dynamique/addArticle.php
+++ site dynamique/addArticle.php
@@ -1,4 +1,5 @@
-<?php session_start(); include('module/dbTools.php'); ?>
+<?php session_start();
+include('module/dbTools.php'); ?>
 <!DOCTYPE html>
 <html>
 
@@ -14,15 +15,60 @@
         include("module/header.php"); ?>
         <section>
             <h2> crée un article</h2>
-                <form action="action.php?action=addArticle" method="post">
-                    <label for="title" >Titre:</label><br/>
-                    <input type="text" name="title" placeholder="Titre" pattern="^.{0,255}" required><br/>
-                    <label for="content" >article:</label><br/>
-                    <textarea name="content" placeholder="article" required></textarea><br/><br/>
-                    <input type="submit">
-                </form>
+            <form action="action.php?action=addArticle" method="post" id="form">
+                <label for="title">Titre:</label><br />
+                <input type="text" name="title" placeholder="Titre" pattern="^.{0,255}" required><br />
+                <label for="content">article:</label><br />
+                <textarea  extarea name="content" placeholder="article" required></textarea><br /><br />
+
+                <!-- file input inserted here-->
+
+                <input type="submit" id="submit">
+            </form>
         </section>
 
+        <!-- template-->
+        <div style="display:none" id="template"> 
+            <div>
+                <input type="file">
+                <button type="button" style="display:none" >supprimer</button>
+                </br>
+            </div>
+        </div>
+        <script>
+            function addNode(){
+                fileEntry = form.insertBefore(template.cloneNode(true),submitButton);
+
+                fileInput = fileEntry.querySelector("input")
+                fileInput.name = "upload_" + nbFile;
+                fileInput.id = "upload_" + nbFile;
+                fileInput.addEventListener('input', change);
+
+                fileButton = fileEntry.querySelector("button")
+                fileButton.id = "delete_" + nbFile;
+                fileButton.addEventListener('click',deleteFile);
+            }
+
+            function change(e) {
+                nbFile += 1;
+                fileName = e.target.name.split("_");
+                document.getElementById("delete_" + fileName[1]).style.display = "inline";
+                addNode();
+            }
+
+            function deleteFile(e) {
+                e.target.parentNode.remove()
+            }
+
+            nbFile = 0
+            form = document.getElementById("form")
+            template = document.getElementById("template").children[0];
+            submitButton = document.getElementById("submit");
+            
+            addNode()
+
+        </script>
+        
     <?php include("module/footer.php");
     }
     ?>
